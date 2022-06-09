39f5711f96c778e0f6f5c00a897b41662e2d8e60
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-06-09 04:55:26 -0700

    simple js declarations

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//global declarations
+let searchHistory = [];
+
+const apiKey = '672c08ae2ee298d34f90eec1a6db0ce9
+';
+const apiUrl = 'https://api.openweathermap.org';
+
+
+//DOM element refs
+const history = document.querySelector('#search-history');
+const searchInput = document.querySelector('#search-input');
+const searchForm = document.querySelector('#search-form');
+const currentCast = document.querySelector('#current-cast');
+const futureCast = document.querySelector('#future-cast');
+
+
